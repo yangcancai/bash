441e746a2e5d263f99ca9af8f4c88666fa6933a9
root <root@li1707-44.members.linode.com>
2020-06-21 02:50:13 +0000

    ssh关闭root和密码登陆

--- user.sh
+++ user.sh
@@ -21,3 +21,7 @@ echo_eval mv /root/.ssh/id_rsa* $ssh_dir/keys
 echo_eval chown -R $u:$u $ssh_dir 
 echo_eval chmod 700 $ssh_dir 
 echo_eval chmod 600 $ssh_dir/authorized_keys
+echo_eval cp /etc/ssh/sshd_config /etc/ssh/sshd_config_bak
+echo_eval sed -i "'s/^PasswordAuthentication yes/PasswordAuthentication no/g'" /etc/ssh/sshd_config
+echo_eval sed -i "'s/^PermitRootLogin yes/PermitRootLogin no/g'" /etc/ssh/sshd_config
+echo_eval service sshd restart
