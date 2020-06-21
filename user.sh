2f35e100b19943b6c2d3daf8f0c66aa65618f215
root <root@li1707-44.members.linode.com>
2020-06-21 05:48:54 +0000

    增加sudo权限for wheel组

--- user.sh
+++ user.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-. ./util.sh
-
+#!/bin/bash 
+. ./util.sh 
 u="$1"
 h=/home/$u
 ssh_dir=$h/.ssh
@@ -16,7 +15,7 @@ echo_eval create_dir $ssh_dir
 echo_eval create_file $ssh_dir/authorized_keys
 echo_eval create_dir $ssh_dir/keys
 echo_eval ssh-keygen
-cat /root/.ssh/id_rsa.pub >> $ssh_dir/authorized_keys
+echo_eval cat /root/.ssh/id_rsa.pub \>\> $ssh_dir/authorized_keys
 echo_eval mv /root/.ssh/id_rsa* $ssh_dir/keys
 echo_eval chown -R $u:$u $ssh_dir 
 echo_eval chmod 700 $ssh_dir 
@@ -25,3 +24,5 @@ echo_eval cp /etc/ssh/sshd_config /etc/ssh/sshd_config_bak
 echo_eval sed -i "'s/^PasswordAuthentication yes/PasswordAuthentication no/g'" /etc/ssh/sshd_config
 echo_eval sed -i "'s/^PermitRootLogin yes/PermitRootLogin no/g'" /etc/ssh/sshd_config
 echo_eval service sshd restart
+echo_eval sed -i "'/wheel/d'" /etc/sudoers
+echo_eval sed -i "'\$a\%wheel ALL=(ALL)       NOPASSWD: ALL'" /etc/sudoers
