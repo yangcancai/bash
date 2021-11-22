d0cd33f392ccef64cc862fa0cb974d359d33d17a
admin <admin@admindeMacBook-Pro.local>
2021-11-22 11:47:06 +0800

    add: tool.sh

--- docker/services/tool.sh
+++ docker/services/tool.sh
@@ -9,6 +9,12 @@ stop(){
 console(){
     docker-compose up
 }
+redis(){
+    docker exec -it dev_redis_latest bash -c "redis-cli -a 123456" 
+}
+mysql(){
+    docker exec -it dev_mysql_latest bash -c "mysql"
+}
 help(){
     echo "sh tool.sh start --- start all service in background mode"
     echo "sh tool.sh stop ---- stop all service"
@@ -18,5 +24,7 @@ case $1 in
 start) start;;
 stop) stop;;
 console) console;;
+redis) redis;;
+mysql) mysql;;
 *) help;;
 esac
