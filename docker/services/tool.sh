9d47ce554fae4a08985c9b6a99bdd8c9cb580f2c
admin <admin@admindeMacBook-Pro.local>
2021-11-22 11:47:45 +0800

    add: tool.sh

--- docker/services/tool.sh
+++ docker/services/tool.sh
@@ -19,6 +19,8 @@ help(){
     echo "sh tool.sh start --- start all service in background mode"
     echo "sh tool.sh stop ---- stop all service"
     echo "sh tool.sh console --- start all service in console mode"
+    echo "sh tool.sh redis ---  redis-cli"
+    echo "sh tool.sh mysql ---  mysql"
 }
 case $1 in
 start) start;;
