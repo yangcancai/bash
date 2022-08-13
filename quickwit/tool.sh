887292bf17acb791e09a0ea53bac3b1cec7bcbf0
Cam <cam@CamdeMacBook-Pro.local>
2022-08-13 18:52:15 +0800

    add vector

--- quickwit/tool.sh
+++ quickwit/tool.sh
@@ -6,11 +6,20 @@ install(){
 start(){
 	docker run -d --name quickwit -v $(pwd)/qwdata:/quickwit/qwdata -p 127.0.0.1:7280:7280 quickwit/quickwit run
 }
+start_vector(){
+	docker run -d --name vector -v $(pwd)/im_container_vector.toml:/etc/vector/vector.toml:ro -p 8383:8383 --add-host=host.docker.internal:host-gateway timberio/vector:0.21.2-distroless-libc
+}
+console_vector(){
+	docker run --init --rm -v $(pwd)/logs:/data/logs -v $(pwd)/im_container_vector.toml:/etc/vector/vector.toml:ro -p 8383:8383 --add-host=host.docker.internal:host-gateway timberio/vector:0.21.2-distroless-libc
+}
 console(){
 	docker run --init --rm -v $(pwd)/qwdata:/quickwit/qwdata -p 127.0.0.1:7280:7280 quickwit/quickwit run
 }
 attach(){
 	docker exec -it quickwit /bin/bash 
+}
+stop_vector(){
+	docker stop vector && docker rm vector
 }
 stop(){
 	 docker stop quickwit && docker rm quickwit
@@ -18,15 +27,26 @@ stop(){
 log(){
 	docker logs quickwit
 }
+create_index(){
+	docker run --rm -it -v $(pwd)/qwdata:/quickwit/qwdata -v $(pwd)/im_container-logs.yaml:/quickwit/index-config.yaml quickwit/quickwit index create --index-config /quickwit/index-config.yaml
+}
+del_index(){
+	docker run --rm -it -v $(pwd)/qwdata:/quickwit/qwdata -v $(pwd)/im_container-logs.yaml:/quickwit/index-config.yaml quickwit/quickwit index  delete --index im_container-logs
+}
 help(){
-	echo "sh tool.sh start|stop|console|install|log|attach"
+	echo "sh tool.sh start|stop|console|install|log|attach|create_index|start_vector|stop_vector|console_vector|del_index"
 }
 case $1 in
 install) install;;
+create_index) create_index;;
 start) start;;
 stop) stop;;
 attach) attach;;
+del_index) del_index;;
 log) log;;
 console) console;;
+start_vector) start_vector;;
+stop_vector) stop_vector;;
+console_vector) console_vector;;
 *)help;;
 esac
